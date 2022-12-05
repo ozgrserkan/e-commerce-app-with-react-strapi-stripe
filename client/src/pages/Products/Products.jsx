1595383233063577aaff17799af663b5d27eaa3f
ozgrserkan <ozgr.serkan09@gmail.com>
2022-12-05 17:22:59 +0300

    react slider added

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./Products.scss";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  return <div className="products">Products</div>;
+};
 
-export default Products
+export default Products;
