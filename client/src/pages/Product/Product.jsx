baa6ac43e0b21a0eff51cdf05fae065ff21484e7
ozgrserkan <ozgr.serkan09@gmail.com>
2022-12-06 18:01:56 +0300

    single product page added

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,8 +1,75 @@
 import React from "react";
+import { useState } from "react";
 import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 const Product = () => {
-  return <div className="product">Product</div>;
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const images = [
+    "https://images.pexels.com/photos/7533345/pexels-photo-7533345.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/9594952/pexels-photo-9594952.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
+
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+          magnam sequi beatae! Iure voluptatum fugit sit tenetur saepe eos,
+          aperiam labore magni temporibus esse possimus iste eligendi, ipsa
+          aliquid velit?
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity(quantity === 1 ? 1 : quantity - 1)}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity(quantity + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CARD
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon />
+            ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor:Polo</span>
+          <span>T-shirt</span>
+          <span>Tag: T-shirt</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFO</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
